cd533b3c40ef4cfcdb2f94007eea7c9bd7d059ef
hwg1111 <1986716843@qq.com>
2024-09-24 14:40:58 +0000

    day4 路由分组

--- gee_web/main.go
+++ gee_web/main.go
@@ -27,26 +27,44 @@ $ curl "http://localhost:9999/xxx"
 404 NOT FOUND: /xxx
 */
 
+/* (5)
+$ curl "http://localhost:9999/xxx"
+404 NOT FOUND: /xxx
+*/
+
+/* (6)
+$ curl "http://localhost:9999/hello"
+404 NOT FOUND: /hello
+*/
+
 func main() {
 	r := gee.New()
-	r.GET("/", func(ctx *gee.Context) { ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>") })
-	r.GET("/hello", func(ctx *gee.Context) {
-		ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Qurey("name"), ctx.Path)
-	})
-	r.POST("/login", func(ctx *gee.Context) {
-		ctx.JSON(http.StatusOK, gee.H{
-			"username": ctx.PostForm("username"),
-			"password": ctx.PostForm("password"),
+	r.GET("/index", func(ctx *gee.Context) { ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>") })
+
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(ctx *gee.Context) {
+			ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 		})
-	})
 
-	r.GET("/hello/:name", func(ctx *gee.Context) {
-		ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Param("name"), ctx.Path)
-	})
+		v1.GET("/hello", func(ctx *gee.Context) {
+			ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Qurey("name"), ctx.Path)
+		})
+	}
 
-	r.GET("/assets/*filepath", func(ctx *gee.Context) {
-		ctx.JSON(http.StatusOK, gee.H{"filepath": ctx.Param("filepath")})
-	})
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(ctx *gee.Context) {
+			ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Param("name"), ctx.Path)
+		})
+
+		v2.POST("/login", func(ctx *gee.Context) {
+			ctx.JSON(http.StatusOK, gee.H{
+				"username": ctx.PostForm("username"),
+				"password": ctx.PostForm("password"),
+			})
+		})
+	}
 
 	r.Run(":9999")
 }
