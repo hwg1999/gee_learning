0123e8d5576d7e648ccca2055f26476790dbcc0b
hwg1111 <1986716843@qq.com>
2024-09-24 15:45:20 +0000

    day7 错误恢复

--- gee_web/main.go
+++ gee_web/main.go
@@ -1,82 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"gee_web/gee"
 	"net/http"
-	"text/template"
-	"time"
 )
 
-/*
-(1) render array
-$ curl http://localhost:9999/date
-<html>
-<body>
-    <p>hello, gee</p>
-    <p>Date: 2019-08-17</p>
-</body>
-</html>
-*/
-
-/*
-(2) custom render function
-$ curl http://localhost:9999/students
-<html>
-<body>
-    <p>hello, gee</p>
-    <p>0: Geektutu is 20 years old</p>
-    <p>1: Jack is 22 years old</p>
-</body>
-</html>
-*/
-
-/*
-(3) serve static files
-$ curl http://localhost:9999/assets/css/geektutu.css
-p {
-    color: orange;
-    font-weight: 700;
-    font-size: 20px;
-}
-*/
-
-type student struct {
-	Name string
-	Age  int8
-}
-
-func FormatAsDate(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
-}
-
 func main() {
-	r := gee.New()
-	r.Use(gee.Logger())
-	r.SetFuncMap(template.FuncMap{
-		"FormatAsDate": FormatAsDate,
-	})
-	r.LoadHTMLGlob("templates/*")
-	r.Static("/assets", "./static")
-
-	stu1 := &student{Name: "Geektutu", Age: 20}
-	stu2 := &student{Name: "Jack", Age: 22}
-	r.GET("/", func(ctx *gee.Context) {
-		ctx.HTML(http.StatusOK, "css.tmpl", nil)
-	})
-	r.GET("/students", func(ctx *gee.Context) {
-		ctx.HTML(http.StatusOK, "arr.tmpl", gee.H{
-			"title":  "gee",
-			"stuArr": [2]*student{stu1, stu2},
-		})
+	r := gee.Default()
+	r.GET("/", func(c *gee.Context) {
+		c.String(http.StatusOK, "Hello Geetutu\n")
 	})
 
-	r.GET("/date", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
-			"title": "gee",
-			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
-		})
+	r.GET("/panic", func(c *gee.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
 	})
 
 	r.Run(":9999")
