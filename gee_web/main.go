f01915b1ae3dc200a9ef4edf1cec5e46b562e4c1
hwg1111 <1986716843@qq.com>
2024-09-24 12:55:03 +0000

    day2 context上下文

--- gee_web/main.go
+++ gee_web/main.go
@@ -1,29 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"gee_web/gee"
 	"net/http"
 )
 
-// $ curl http://localhost:9999/
-// URL.Path = "/"
-// $ curl http://localhost:9999/hello
-// Header["Accept"] = ["*/*"]
-// Header["User-Agent"] = ["curl/7.54.0"]
-// curl http://localhost:9999/world
-// 404 NOT FOUND: /world
+/*
+(1)
+$ curl -i http://localhost:9999/
+HTTP/1.1 200 OK
+Date: Mon, 12 Aug 2019 16:52:52 GMT
+Content-Length: 18
+Content-Type: text/html; charset=utf-8
+<h1>Hello Gee</h1>
+
+(2)
+$ curl "http://localhost:9999/hello?name=geektutu"
+hello geektutu, you're at /hello
+
+(3)
+$ curl "http://localhost:9999/login" -X POST -d 'username=geektutu&password=1234'
+{"password":"1234","username":"geektutu"}
+
+(4)
+$ curl "http://localhost:9999/xxx"
+404 NOT FOUND: /xxx
+*/
 
 func main() {
 	r := gee.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(ctx *gee.Context) { ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>") })
+	r.GET("/hello", func(ctx *gee.Context) {
+		ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Qurey("name"), ctx.Path)
 	})
-
-	r.GET("/hello", func(w http.ResponseWriter, r *http.Request) {
-		for k, v := range r.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.POST("/login", func(ctx *gee.Context) {
+		ctx.JSON(http.StatusOK, gee.H{
+			"username": ctx.PostForm("username"),
+			"password": ctx.PostForm("password"),
+		})
 	})
 
 	r.Run(":9999")
