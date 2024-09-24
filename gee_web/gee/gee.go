27bf3deb562663d884456db39d18a629f40efef7
hwg1111 <1986716843@qq.com>
2024-09-24 15:30:32 +0000

    day6 模板引擎

--- gee_web/gee/gee.go
+++ gee_web/gee/gee.go
@@ -3,7 +3,9 @@ package gee
 import (
 	"log"
 	"net/http"
+	"path"
 	"strings"
+	"text/template"
 )
 
 type HandlerFunc func(*Context)
@@ -18,8 +20,10 @@ type (
 
 	Engine struct {
 		*RouterGroup
-		router *router
-		groups []*RouterGroup
+		router        *router
+		groups        []*RouterGroup
+		htmlTemplates *template.Template
+		funcMap       template.FuncMap
 	}
 )
 
@@ -59,6 +63,34 @@ func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
 	group.addRoute("POST", pattern, handler)
 }
 
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(ctx *Context) {
+		file := ctx.Param("filepath")
+		if _, err := fs.Open(file); err != nil {
+			ctx.Status(http.StatusNotFound)
+			return
+		}
+
+		fileServer.ServeHTTP(ctx.Writer, ctx.Req)
+	}
+}
+
+func (group *RouterGroup) Static(relativePath string, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	group.GET(urlPattern, handler)
+}
+
+func (engine *Engine) SetFuncMap(funcMap template.FuncMap) {
+	engine.funcMap = funcMap
+}
+
+func (engine *Engine) LoadHTMLGlob(pattern string) {
+	engine.htmlTemplates = template.Must(template.New("").Funcs(engine.funcMap).ParseGlob(pattern))
+}
+
 func (engine *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
 }
@@ -72,5 +104,6 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 	c := newContext(w, req)
 	c.handlers = middlewares
+	c.engine = engine
 	engine.router.handle(c)
 }
--- gee_web/main.go
+++ gee_web/main.go
@@ -1,54 +1,83 @@
 package main
 
 import (
+	"fmt"
 	"gee_web/gee"
-	"log"
 	"net/http"
+	"text/template"
 	"time"
 )
 
 /*
-(1) global middleware Logger
-$ curl http://localhost:9999/
-<h1>Hello Gee</h1>
-
->>> log
-2019/08/17 01:37:38 [200] / in 3.14µs
+(1) render array
+$ curl http://localhost:9999/date
+<html>
+<body>
+    <p>hello, gee</p>
+    <p>Date: 2019-08-17</p>
+</body>
+</html>
 */
 
 /*
-(2) global + group middleware
-$ curl http://localhost:9999/v2/hello/geektutu
-{"message":"Internal Server Error"}
-
->>> log
-2019/08/17 01:38:48 [200] /v2/hello/geektutu in 61.467µs for group v2
-2019/08/17 01:38:48 [200] /v2/hello/geektutu in 281µs
+(2) custom render function
+$ curl http://localhost:9999/students
+<html>
+<body>
+    <p>hello, gee</p>
+    <p>0: Geektutu is 20 years old</p>
+    <p>1: Jack is 22 years old</p>
+</body>
+</html>
 */
 
-func onlyForV2() gee.HandlerFunc {
-	return func(ctx *gee.Context) {
-		t := time.Now()
-		ctx.Fail(500, "Internal Server Error")
-		log.Printf("[%d] %s in %v for group v2", ctx.StatusCode, ctx.Req.RequestURI, time.Since(t))
-	}
+/*
+(3) serve static files
+$ curl http://localhost:9999/assets/css/geektutu.css
+p {
+    color: orange;
+    font-weight: 700;
+    font-size: 20px;
+}
+*/
+
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
 }
 
 func main() {
 	r := gee.New()
 	r.Use(gee.Logger())
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
+
+	stu1 := &student{Name: "Geektutu", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
 	r.GET("/", func(ctx *gee.Context) {
-		ctx.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		ctx.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(ctx *gee.Context) {
+		ctx.HTML(http.StatusOK, "arr.tmpl", gee.H{
+			"title":  "gee",
+			"stuArr": [2]*student{stu1, stu2},
+		})
 	})
 
-	v2 := r.Group("/v2")
-	v2.Use(onlyForV2())
-	{
-		v2.GET("/hello/:name", func(c *gee.Context) {
-			// except /hello/geetutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	r.GET("/date", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
+			"title": "gee",
+			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
 		})
-	}
+	})
 
 	r.Run(":9999")
 }
