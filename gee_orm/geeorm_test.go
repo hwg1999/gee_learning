016ec7410e81ae9f9d41cab40feb0644931f4eca
hwg1111 <1986716843@qq.com>
2024-09-26 16:12:55 +0000

    day4 链式操作和更新删除

--- gee_orm/geeorm_test.go
+++ gee_orm/geeorm_test.go
@@ -1,6 +1,7 @@
 package geeorm
 
 import (
+	"fmt"
 	"testing"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -18,4 +19,10 @@ func OpenDB(t *testing.T) *Engine {
 func TestNewEngine(t *testing.T) {
 	engine := OpenDB(t)
 	defer engine.Close()
+	s := engine.NewSession()
+	_, _ = s.Raw("DROP TABLE IF EXISTS User;").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text);").Exec()
+	result, _ := s.Raw("INSERT INTO User(`Name`) values (?), (?)", "Tom", "Sam").Exec()
+	count, _ := result.RowsAffected()
+	fmt.Printf("Exec success, %d affected\n", count)
 }
